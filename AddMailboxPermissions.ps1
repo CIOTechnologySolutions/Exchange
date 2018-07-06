9e19a60da99b277a6d7cdb521f677fea84881f91
Kyle Elliott <kelliott@ciotech.us>
2018-07-06 15:50:45 -0400

    Remove popup boxes, convert to passable parameters

--- AddMailboxPermissions.ps1
+++ AddMailboxPermissions.ps1
@@ -1,5 +1,27 @@
-$Mailbox1 = read-host -Prompt 'Input email address you need access to'
-$Mailbox2 = read-host -Prompt 'Input email address to grant rights to'
+#AddMailboxPermissions.ps1
+<#
+.Synopsis
+    Set full permissions on a mailbox
+.parameter target
+      Enter the mailbox to be modified
+.Parameter user
+      Enter the user that will need access to the target mailbox
+.Example
+      AddMailboxPermissions -target Jim -user frank
+.NOTES
+      Created to help guide a lower level tech through how to add full access
+      Provided without any warranty and entirely as is.
+      By Kyle Elliott
+      kelliott(at)ciotech(dot)us
+#>
+{
+param(
+  [Parameter(Mandatory=$true)]
+  [string]$target,
+  [Parameter(Mandatory=$True)]
+  [string]$user
+)
 
-Add-MailboxPermission -Identity $Mailbox1 -User $Mailbox2 -AccessRights 'FullAccess'
-write-host "Access to '$mailbox1' has been granted to '$mailbox2'"
+Add-MailboxPermission -Identity $target -User $user -AccessRights 'FullAccess'
+write-host "Access to '$target' has been granted to '$user'"
+}
