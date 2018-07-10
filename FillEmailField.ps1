b1e1240ed4abd5f7dfdd9feef82c8b44f585c9a2
Kyle Elliott <kelliott@ciotech.us>
2018-07-10 15:11:12 -0400

    Created help block

--- FillEmailField.ps1
+++ FillEmailField.ps1
@@ -1,3 +1,19 @@
+<#Windows PowerShell Code
+.description
+Scipt to fill out the email field in AD from CSV - FillEmailField.ps1
+.Synopsis
+This script runs through your CSV, and fills out the email address field on any user listed
+CSV must contain the following headers:
+sAMAccountName,EmailAddress
+.Parameter CSV
+Provide path to CSV File that contains a header of sAMAccountName
+.Example
+FillEmailField.ps1 -csv c:\temp\emailaddress.csv
+.NOTES
+  By Kyle Elliott
+  kelliott(at)ciotech(dot)us
+  Provided as is, without warranty
+  #>
 ﻿{
 Param(
     [Parameter(Mandatory=$true)]
